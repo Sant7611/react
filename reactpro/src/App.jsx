6d22a938045568f6037b768cdf37f57729efe5f6
santosh <sbohara579@gmail.com>
2024-07-05 18:25:12 +0545

    conditional rendering, calling component using component

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,10 +1,13 @@
+import ConditionalComponent from "./components/ConditionalComponent";
+import Fruits from "./components/Fruits";
 import Hello from "./components/Hello";
 function App() {
   return (
       <div className="App">
-        <Hello named = "Sajina" message = "Hi there "/>
+        {/* <Hello named = "Sajina" message = "Hi there "/> */}
+        {/* <Fruits /> */}
+        <ConditionalComponent />
       </div>
-   
   );
 }
 
