30d8b53851f65974e1d62b649c41eaed04691d87
santosh <sbohara579@gmail.com>
2024-07-09 21:19:48 +0545

    just

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         {/* <ConditionalComponent /> */}
         {/* <Event /> */}
         {/* <Counter /> */}
-        {/* <Form /> */}
+        <Form />
         <MultipleForms />
       </div>
   );
